a9f8935e88458463cb471ee4930305ed8ff9e07b
kay-apao25 <kayapao@yahoo.com>
2016-10-27 16:22:03 +0800

    Remove unnecessary line

--- src/component/example.jsx
+++ src/component/example.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 
 export default class Example extends React.Component{
   render(){
-    const { errorMsg } = this.props
-
     return (<div>
               "Hello World!"
             </div>
